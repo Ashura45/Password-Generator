8cbd78f2df0850fee5fd59e265862ad839f54e43
Алеша <144483223+Ashura45@users.noreply.github.com>
2024-07-07 06:27:45 +0500

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -1,6 +1,10 @@
 import random
+
+
 def generate_it_again():
     pass
+
+
 def opc():
     long = int(input('Напишите длину пароля: '))
     with_num = input('Пароль будет с цифрами? y or n: ').lower()
@@ -13,17 +17,19 @@ def opc():
             with_num = input('Пароль будет с цифрами? y or n: ').lower()
             with_letters = input('Пароль будет с буквами? y or n: ').lower()
     return long, with_num == 'y', with_letters == 'y'
+
+
 def password_generator():
     long, with_num, with_letters = opc()
     password = ''
     if with_num and with_letters:
         for i in range(long):
-            j = random.randint(0,1)
-            j_ = random.randint(0,1)
+            j = random.randint(0, 1)
+            j_ = random.randint(0, 1)
             if j == 1:
-                x = random.randint(48,57)
+                x = random.randint(48, 57)
             else:
-                x = random.randint(97,122)
+                x = random.randint(97, 122)
             if j_ == 1:
                 password = password + chr(x).upper()
             else:
@@ -31,18 +37,21 @@ def password_generator():
 
     elif with_letters:
         for i in range(long):
-            j = random.randint(0,1)
-            x = random.randint(97,122)
+            j = random.randint(0, 1)
+            x = random.randint(97, 122)
             if j == 1:
                 password = password + chr(x).upper()
             else:
                 password = password + chr(x)
     elif with_num:
         for i in range(long):
-            x = random.randint(48,57)
+            x = random.randint(48, 57)
             password = password + chr(x)
 
     return password
+
+
 print(password_generator())
+
 
 
